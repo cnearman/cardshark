94dc9c704977f56a3ae87684e9fe6628f8c77d28
Chris Nearman <nearman.cj@gmail.com>
2021-07-06 00:31:20 -0700

    Added join_channel signal for initiating p2p connection.

--- app.js
+++ app.js
@@ -13,8 +13,38 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
+var sockets = {};
+var channels = {};
+
 io.on("connection", (socket) => {
-    console.log("Client connected.");
+    socket.channels = {};
+    console.log(`Client connected. Socket Id: ${socket.id}`);
+    sockets[socket.id] = socket;
+
+    socket.on('join_channel', (config) => {
+        var channel = config.channel;
+
+        if (channel in socket.channels){
+            // already joined
+            return;
+        }
+
+        if (!(channel in channels)){
+            channels[channel] = {};
+        }
+
+        for (id in channels[channel]){
+            channels[channel][id].emit('begin_peer_connection', {'peer_socket_id': socket.id, 'should_create_offer' : false});
+            socket.emit('begin_peer_connection', {'peer_socket_id':id, 'should_create_offer' : true});
+        }
+
+        channels[channel][socket.id] = socket;
+        socket.channels[channel] = channel;
+    });
+
+    socket.on('disconnect', function (){
+        console.log(`Client disconnected. Socket Id: ${socket.id}`)
+    });
 })
 
 server.listen(port, () => console.log(`Express App running on port ${port}`));
